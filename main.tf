30ecbc78fd190420639530f1e24546708bcd2389
Sai <Vgoud5959@gmail.com>
2022-12-01 22:01:53 +0530

    added

--- main.tf
+++ main.tf
@@ -23,10 +23,7 @@ terraform {
 provider "aws" {
   region = "us-east-1"
 }
-variable "app_count" {
-  type = number
-  default = 1
-}
+
 data "aws_availability_zones" "available_zones" {
   state = "available"
 }
