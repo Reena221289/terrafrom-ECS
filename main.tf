d6f7098dcfb213b53c903bc6e9e1e5862a06d007
Vamshi_Kesari <118153587+kesarivamshi@users.noreply.github.com>
2022-12-03 18:51:05 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -110,15 +110,15 @@ resource "aws_ecs_task_definition" "hello_world" {
   container_definitions = <<DEFINITION
 [
   {
-    "image": "snipe/snipe-it",
+    "image": "linuxserver/snipe-it",
     "cpu": 256,
     "memory": 512,
     "name": "hello-world-app",
     "networkMode": "awsvpc",
     "portMappings": [
       {
-        "containerPort": 8000,
-        "hostPort": 8000
+        "containerPort": 8080,
+        "hostPort": 8080
       }
     ]
   }
@@ -131,8 +131,8 @@ resource "aws_security_group" "hello_world_task" {
 
   ingress {
     protocol        = "tcp"
-    from_port       = 8000
-    to_port         = 8000
+    from_port       = 8080
+    to_port         = 8080
     security_groups = [aws_security_group.lb.id]
   }
 
@@ -162,7 +162,7 @@ resource "aws_ecs_service" "hello_world" {
   load_balancer {
     target_group_arn = aws_lb_target_group.hello_world.id
     container_name   = "hello-world-app"
-    container_port   = 8000
+    container_port   = 8080
   }
 
   depends_on = [aws_lb_listener.hello_world]
