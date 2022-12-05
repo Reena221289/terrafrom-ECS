893e1948f1e94ddf02325740f2d5acdbf7147de2
Reena221289 <61221079+Reena221289@users.noreply.github.com>
2022-12-05 20:23:49 +0530

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -1,9 +1,11 @@
 terraform {
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
-      version = "3.26.0"
+      source = "hashicorp/aws"
+      version = "4.45.0"
     }
+  }
+}
     random = {
       source  = "hashicorp/random"
       version = "3.0.1"
